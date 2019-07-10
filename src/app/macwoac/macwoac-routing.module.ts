ab03bec8a0850c620a9824ec2c7ee7e5423fb448
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-07-10 13:19:04 +0530

    routing

--- src/app/macwoac/macwoac-routing.module.ts
+++ src/app/macwoac/macwoac-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent }
+  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
